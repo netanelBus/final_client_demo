572d144cc2724afa98509b81bbaa6be64d43fad6
natiBus <natimogli@gmail.com>
2023-07-16 13:02:58 +0200

    init firebase

---
+++ src/firebaseStuff.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwzw7vO8PaB0HJZYX5dyYLOeNWYoDrF2M",
+  authDomain: "final-demo-79469.firebaseapp.com",
+  projectId: "final-demo-79469",
+  storageBucket: "final-demo-79469.appspot.com",
+  messagingSenderId: "496324402571",
+  appId: "1:496324402571:web:bfe446b0ef309c5d15e38e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
